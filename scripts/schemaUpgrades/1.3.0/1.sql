29b555db5638aa9bffab0e320e06be9bbb093945
Ryan Donnelly <ryanjdonnelly@gmail.com>
2013-04-07 18:21:03 -0400

    added sql to import old registrations

--- scripts/schemaUpgrades/1.3.0/1.sql
+++ scripts/schemaUpgrades/1.3.0/1.sql
@@ -16,6 +16,9 @@ CREATE TABLE IF NOT EXISTS `registrations` (
   `waitlist` BOOLEAN NOT NULL DEFAULT 0,
   `attendance` int(2) NULL DEFAULT NULL,
   `captain` int(2) NULL DEFAULT NULL,
+  `guardian` BOOLEAN NULL DEFAULT NULL,
+  `guardian_name` VARCHAR(255) NULL DEFAULT NULL,
+  `guardian_email` VARCHAR(255) NULL DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `league_id` (`league_id`),
   KEY `player_id` (`user_id`),
@@ -26,3 +29,28 @@ ALTER TABLE `registrations`
   ADD CONSTRAINT `registrations_ibfk_1` FOREIGN KEY (`league_id`) REFERENCES `league` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `registrations_ibfk_2` FOREIGN KEY (`baggage_id`) REFERENCES `baggage` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `registrations_ibfk_3` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+
+INSERT INTO `registrations` (`id`, `user_id`, `league_id`, `baggage_id`, `created`, `updated`, `conduct_complete`, `waiver_complete`, `pay_type`, `check_complete`, `paypal_invoice_id`, `paypal_complete`, `waitlist`, `attendance`, `captain`, `guardian`, `guardian_name`, `guardian_email`)
+
+SELECT
+NULL,
+user_id,
+league_id,
+baggage_id,
+reg_time,
+reg_time,
+status NOT IN ('new'),
+status NOT IN ('new', 'clicked_waiver'),
+IF(status LIKE '%paypal%', 'paypal', IF(status LIKE '%check%', 'check', NULL)),
+status IN ('check_completed', 'check-completed_waitlist'),
+NULL,
+status IN ('paypal_completed', 'paypal-completed_waitlist'),
+status LIKE '%waitlist%',
+attendance,
+captaining,
+`guardian_waiver`,
+`guardian_name`,
+`guardian_email`
+FROM registration
+WHERE league_id != 0;
