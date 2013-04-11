2608befd3c5ede04c9fa8b392a7cd9f7603dec29
Ryan Donnelly <ryanjdonnelly@gmail.com>
2013-04-11 00:22:57 -0400

    added template tag to detect league deadlines fixed freeze date check for baggage to go through date (not end on)

--- ultimate/templatetags/leagues.py
+++ ultimate/templatetags/leagues.py
@@ -1,6 +1,12 @@
+from datetime import date
 from django import template
 
 register = template.Library()
+
+
+@register.filter
+def is_past_deadline(league_date):
+	return bool(date.today() > league_date)
 
 
 @register.filter
@@ -13,6 +19,7 @@ def get_league_captains(league):
 # returns league captains as teammember objects
 def get_league_captains_teammember(league):
 	return league.get_league_captains_teammember()
+
 
 @register.filter
 # returns whether a user has filled out a player survey for a league
