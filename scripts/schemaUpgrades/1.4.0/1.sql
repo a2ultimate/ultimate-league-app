09373de5b7e21788480bd67c2b4498aa66fe9ceb
Ryan Donnelly <ryanjdonnelly@gmail.com>
2013-09-12 10:57:19 -0400

    removed all schema references as they are no longer relavant (already migrated)

--- scripts/schemaUpgrades/1.4.0/1.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO schema_version (major, middle, minor) VALUES (1, 4, 1);
-
-ALTER TABLE  `player` ADD  `time_zone` DOUBLE NOT NULL ,
-ADD  `language` VARCHAR( 10 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL ,
-ADD  `post_count` INT( 11 ) NOT NULL ,
-ADD  `avatar` VARCHAR( 100 ) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL ,
-ADD  `autosubscribe` TINYINT( 1 ) NOT NULL DEFAULT TRUE;
