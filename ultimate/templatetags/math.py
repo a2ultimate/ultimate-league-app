5c61aaabaf91dd10e544fceb6c72caf9aa801038
Ryan Donnelly <ryanjdonnelly@gmail.com>
2013-09-13 01:03:09 -0400

    [fix] registration progress bar is now filled up to current step renamed math_tags template tag file to math

---
+++ ultimate/templatetags/math.py
@@ -0,0 +1,27 @@
+from django import template
+
+register = template.Library()
+
+@register.filter
+def add(value, arg):
+	return int(value) + int(arg)
+
+@register.filter
+def subtract(value, arg):
+	return int(value) - int(arg)
+
+@register.filter
+def divide(value, arg):
+	return int(value) / int(arg)
+
+@register.filter
+def divide_by_count(value, arg):
+	return int(value) / int(len(arg))
+
+@register.filter
+def limit_ceil(value, arg):
+	return min(int(value), int(arg))
+
+@register.filter
+def limit_floor(value, arg):
+	return max(int(value), int(arg))
