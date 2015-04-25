6826735ad5df554fbd410b8c4f1d264ffbb82f71
Ryan Donnelly <ryanjdonnelly@gmail.com>
2015-04-25 18:37:10 -0400

    fix import error with IntegrityError

--- ultimate/leagues/views.py
+++ ultimate/leagues/views.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.urlresolvers import reverse
+from django.db import IntegrityError
 from django.http import HttpResponseRedirect
 from django.shortcuts import get_object_or_404, redirect, render_to_response
 from django.template import RequestContext
