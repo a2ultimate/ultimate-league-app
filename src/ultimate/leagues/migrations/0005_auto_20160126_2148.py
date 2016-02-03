916644011669a96084957f436313ade7a12a8786
Ryan Donnelly <ryanjdonnelly@gmail.com>
2016-02-02 21:24:36 -0500

    update league schema

---
+++ src/ultimate/leagues/migrations/0005_auto_20160126_2148.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('leagues', '0004_auto_20151105_2210'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='league',
+            name='gender',
+            field=models.CharField(max_length=32, choices=[(b'50/50', b'50/50 League'), (b'co-ed', b'Normal Co-Ed Gender Matched'), (b'competitive', b'Competitive League'), (b'event', b'Special Event'), (b'hat', b'Hat Tourney'), (b'open', b'Open, No Gender Match'), (b'showcase', b'Showcase League'), (b'women', b'Women Only')]),
+        ),
+        migrations.AlterField(
+            model_name='player',
+            name='date_of_birth',
+            field=models.DateField(help_text=b'e.g. 2016-01-26'),
+        ),
+    ]
