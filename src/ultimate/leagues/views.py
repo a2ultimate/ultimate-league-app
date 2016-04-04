c86264eb8d5069b227058d83f80cb546971cc8b2
Ryan Donnelly <ryanjdonnelly@gmail.com>
2016-04-03 21:40:31 -0400

    fix issue with users without a profile

--- src/ultimate/leagues/views.py
+++ src/ultimate/leagues/views.py
@@ -83,9 +83,9 @@ def players(request, year, season, division):
 			'refunded_registrations': refunded_registrations,
 			'unassigned_registrations': unassigned_registrations,
 
-			'registrations_female': len([r for r in complete_registrations if r.user.profile.is_female()]),
-			'registrations_male': len([r for r in complete_registrations if r.user.profile.is_male()]),
-			'registrations_minor': len([r for r in complete_registrations if r.user.profile.is_minor(league.league_start_date)]),
+			'registrations_female': len([r for r in complete_registrations if hasattr(r.user, 'profile') and r.user.profile.is_female()]),
+			'registrations_male': len([r for r in complete_registrations if hasattr(r.user, 'profile') and r.user.profile.is_male()]),
+			'registrations_minor': len([r for r in complete_registrations if hasattr(r.user, 'profile') and r.user.profile.is_minor(league.league_start_date)]),
 			'registrations_remaining': max(0, league.max_players - len(complete_registrations)),
 		},
 		context_instance=RequestContext(request))
