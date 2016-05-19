b84343688ee7bc8f2adf5b0ef2ec512d0d529d3a
Ryan Donnelly <ryanjdonnelly@gmail.com>
2016-05-19 12:24:52 -0400

    fix migrations

--- src/ultimate/leagues/migrations/0023_coupon.py
+++ src/ultimate/leagues/migrations/0023_coupon.py
@@ -9,7 +9,7 @@ class Migration(migrations.Migration):
 
     dependencies = [
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
-        ('leagues', '0022_auto_20160327_1916'),
+        ('leagues', '0022_auto_20160519_1150'),
     ]
 
     operations = [
