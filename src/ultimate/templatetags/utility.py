8fe5fede4429f3623c9fe244ad13e0d9441b33a5
Ryan Donnelly <ryanjdonnelly@gmail.com>
2016-10-31 17:16:07 -0400

    fix up some styles on players and teams pages

--- src/ultimate/templatetags/utility.py
+++ src/ultimate/templatetags/utility.py
@@ -12,8 +12,7 @@ register = template.Library()
 
 @register.filter()
 def groups_sort(groups):
-    groups.sort(key=lambda group: len(group['list']))
-    return groups
+    return sorted(groups, key=lambda group: len(group['list']))
 
 
 @register.filter
