67b7ea0305144999607df7bc22729dd7f2d2684a
Ryan Donnelly <ryanjdonnelly@gmail.com>
2019-05-09 21:49:26 -0400

    clean up some dependencies

--- news-migrate.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO news_article (title, type, content, created, published, updated)
-SELECT pybb_topic.name, 'html', pybb_post.body_html, pybb_post.created, pybb_post.created, pybb_post.created
-FROM pybb_post
-JOIN pybb_topic ON pybb_topic.id = pybb_post.topic_id
-JOIN pybb_forum ON pybb_forum.id = pybb_topic.forum_id
-WHERE pybb_forum.name = 'Announcements';
