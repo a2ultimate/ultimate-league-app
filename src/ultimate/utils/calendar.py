d3fede5e284ace7510342c21a756609b6d0fa3d3
Ryan Donnelly <ryanjdonnelly@gmail.com>
2020-12-19 21:34:01 -0500

    protect against failed google api requests

--- src/ultimate/utils/calendar.py
+++ src/ultimate/utils/calendar.py
@@ -7,13 +7,15 @@ from ultimate.utils.google_api import GoogleAppsApi
 
 def get_events():
     calendar_id = getattr(settings, 'GOOGLE_APPS_CALENDAR_ID', False)
-    events = []
+    events = None
     since = timedelta(weeks=0)
     until = timedelta(weeks=12)
 
-    if calendar_id:
-        api = GoogleAppsApi()
-
-        events = api.get_calendar_events(calendar_id, since, until)
+    try:
+        if calendar_id:
+            api = GoogleAppsApi()
+            events = api.get_calendar_events(calendar_id, since, until)
+    except:
+        events = None
 
     return events
