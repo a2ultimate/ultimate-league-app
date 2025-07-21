81bee78d1a0ddee7e93f31e5b567cb8960f59ec0
Ryan Donnelly <ryanjdonnelly@gmail.com>
2025-07-21 17:36:09 -0400

    add sleep for email address add

--- src/ultimate/utils/email_groups.py
+++ src/ultimate/utils/email_groups.py
@@ -1,21 +1,33 @@
+import time
 from ultimate.utils.google_api import GoogleAppsApi
 
 
-def add_to_group(group_id=None, group_email_address=None, email_address=None, file_path=None):
+def add_to_group(
+    group_id=None, group_email_address=None, email_address=None, file_path=None
+):
     api = GoogleAppsApi()
     success_count = 0
 
     if email_address:
-        if api.add_group_member(email_address, group_id=group_id, group_email_address=group_email_address):
+        time.sleep(2)
+        if api.add_group_member(
+            email_address,
+            group_id=group_id,
+            group_email_address=group_email_address,
+        ):
             success_count = success_count + 1
 
     elif file_path:
         try:
-            opened_file = open(file_path, 'r')
+            opened_file = open(file_path, "r")
             email_list = opened_file.read().splitlines()
 
             for email_address in email_list:
-                if api.add_group_member(email_address, group_id=group_id, group_email_address=group_email_address):
+                if api.add_group_member(
+                    email_address,
+                    group_id=group_id,
+                    group_email_address=group_email_address,
+                ):
                     success_count = success_count + 1
 
         finally:
@@ -26,56 +38,59 @@ def add_to_group(group_id=None, group_email_address=None, email_address=None, fi
 
 
 def generate_email_list_address(league, team=None, suffix=None):
-    if league.type == 'league':
+    if league.type == "league":
         descriptor = league.level
-        if not league.gender == 'corec':
-            descriptor = '{}-{}'.format(league.level, league.gender)
+        if not league.gender == "corec":
+            descriptor = "{}-{}".format(league.level, league.gender)
 
-        group_address = '{}{}-{}-{}'.format(
+        group_address = "{}{}-{}-{}".format(
             league.season.slug,
             str(league.year)[-2:],
-            league.league_start_date.strftime('%a'),
+            league.league_start_date.strftime("%a"),
             descriptor,
         )
     else:
-        group_address = '{}{}-{}'.format(
+        group_address = "{}{}-{}".format(
             league.season.slug,
             str(league.year)[-2:],
             league.night_slug,
         )
 
     if team is not None:
-        group_address += '-' + str(team.id)
+        group_address += "-" + str(team.id)
 
     if suffix is not None:
-        group_address += '-' + suffix
+        group_address += "-" + suffix
 
-    return '{}@lists.annarborultimate.org'.format(group_address).lower()
+    return "{}@lists.annarborultimate.org".format(group_address).lower()
 
 
 def generate_email_list_name(league, team=None, suffix=None):
-    if league.type == 'league':
+    if league.type == "league":
         descriptor = league.display_level
-        if not league.gender == 'corec':
-            descriptor = '{} {}'.format(league.display_level, league.display_gender)
+        if not league.gender == "corec":
+            descriptor = "{} {}".format(
+                league.display_level,
+                league.display_gender,
+            )
 
-        group_name = '{} {} {} {}'.format(
+        group_name = "{} {} {} {}".format(
             league.season.name,
             league.year,
-            league.league_start_date.strftime('%A'),
+            league.league_start_date.strftime("%A"),
             descriptor,
         )
     else:
-        group_name = '{} {} {}'.format(
+        group_name = "{} {} {}".format(
             league.season.name,
             league.year,
             league.night,
         )
 
     if team is not None:
-        group_name += ' - Team ' + str(team.id)
+        group_name += " - Team " + str(team.id)
 
     if suffix is not None:
-        group_name += ' - ' + suffix
+        group_name += " - " + suffix
 
     return group_name
